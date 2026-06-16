94beb5c35461de0b40ffcb06d012d4f91ad3cc27
Lizo <lizabuturskaa96@gmail.com>
2026-06-17 05:03:35 +0900

    Обновление стилей админ-панели и правки шаблонов

--- verenn/settings.py
+++ verenn/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)n%u9^)ftn3^99x64a17-(r0sx+*j^)nqs--909)yy9nowdbv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['LawnBoo://verennik.ru']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
